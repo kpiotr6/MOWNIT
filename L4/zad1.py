7d118b877f7b0bc0f6c43af7b8606f9d4c27af44
kpiotr6 <piotrek@komlan.pl>
2023-03-29 13:18:16 +0200

    L4 another

--- L4/zad1.py
+++ L4/zad1.py
@@ -1,7 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+def f1(x):
+    return 1/(1+25*x*x)
 
-def lagrange_polynomial_standard(x_values, y_values, x):
+def f2(x):
+    return np.exp(np.cos(x))
+
+def lagrange_polynomial(x_values, y_values, x):
     sol = 0
     t = 0
     for i in range(0, len(x_values)):
@@ -24,7 +30,7 @@ def get_nodes_cheb(n):
     nodes = np.empty(shape=n, dtype=float)
     for i in range(n):
         nodes[i] = np.cos(np.pi*(2*i+1)/(2*n+2))
-    return nodes[i]
+    return nodes
 
 #gets n equidistant nodes in interval [a,b] 
 
@@ -38,7 +44,7 @@ def get_eqdist(n, a, b):
 
 #calculates sigmas in matrix equation (strona 17)
 
-def get_qubic_sigmar(nodes, y_values):
+def qubic_simas_hs(nodes, y_values):
     n = len(nodes) - 1
     hs = np.zeros(shape=n)
     for i in range(n):
@@ -59,7 +65,7 @@ def get_qubic_sigmar(nodes, y_values):
     d_matrix[n] = -hs[n-1]*hs[n-1]*b[3]
     for i in range(1,n):
         d_matrix[i] = (y_values[i+1]-y_values[i])/hs[i] - (y_values[i]-y_values[i-1])/hs[i-1]
-    return np.linalg.solve(h_matrix,d_matrix)
+    return np.linalg.solve(h_matrix,d_matrix),hs
 
 #calculates those fucked up deltas raised to power
 
@@ -76,8 +82,34 @@ def coefficient(x, y):
         a[k:m] = (a[k:m] - a[k - 1])/(x[k:m] - x[k - 1])
     return a
 
+def spline_value(nodes, y_values, sigmas, hs, x):
+    n = len(nodes)
+    if x < nodes[0] or x > nodes[n-1]:
+        return None
+    for i in range(0,n-1):
+        if nodes[i] <= x <= nodes[i+1]:
+            k = i
+    b = (y_values[k+1]-y_values[k])-hs[k]*(sigmas[k+1]+2*sigmas[k])
+    c = 3*sigmas[k]
+    d = (sigmas[k+1]-sigmas[k])/hs[k]
+    return y_values[k] + b*(x-nodes[k]) + c*((x-nodes[k])**2) + d*((x-nodes[k])**3)
+
 if __name__ == "__main__":
-    x = [1,2,3,4,5,6,7,8,9,10]
-    y =[30,50,70,75,78,80,90,100,105,110]
-    print(get_qubic_sigmar(x,y))
+    # sig, hs = qubic_simas_hs(x, y)
+    # print(spline_value(x,y,sig,hs,))
+    # mat
+    x_eq = get_eqdist(12,-1,1)
+    y_eq = [f1(x) for x in x_eq]
+    x_ch = get_nodes_cheb(12)
+    y_ch = [f1(x) for x in x_ch]
+    sig, hs = qubic_simas_hs(x_eq, y_eq)
+    
+    x_space = np.arange(-1, 1, 1/60)
+    y_edist_spl = [spline_value(x_eq,y_eq,sig,hs,x) for x in x_space]
+    y_eqdist_lag = [lagrange_polynomial(x_eq,y_eq,x) for x in x_space]
+    y_cheb_lag = [lagrange_polynomial(x_ch,y_ch,x) for x in x_space]
+    # plt.plot(x_space,y_eqdist_lag)
+    plt.plot(x_space,y_edist_spl)
+    # plt.plot(x_space,[f1(x) for x in x_space])
+
     pass
